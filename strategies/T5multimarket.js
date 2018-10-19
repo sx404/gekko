1fc8dbbc70a52ec79947c187c51cc369bea685e4
mark.sch <mark@schmalohr.de>
2018-10-19 18:56:12 +0200

    Updated multimarket strategy for container compatibility

--- strategies/T5multimarket.js
+++ strategies/T5multimarket.js
@@ -20,65 +20,82 @@ const adapter = config[config.adapter];
 const Reader = require('../' + adapter.path + '/reader.js');
 const TALIBASYNC = require('../strategies/indicators/TalibAsync.js');
 
-var stratFlash = {};
+var stratMM = {};
 
 
-stratFlash.init = function () {
+// ***************************************************************************
+// * Init a pice percent oszillator for bitcoin and altcoin market
+stratMM.init = function () {
     this.name = 'T5 multi market analyzer (BTC)';
 
     this.reader = new Reader('binance');
 
-    this.ppo = new TALIBASYNC({ indicator: 'ppo', length: 3, options: { optInFastPeriod: 3, optInSlowPeriod: 2, optInMAType: 0 } });
-    this.ppoFC = new TALIBASYNC({ indicator: 'ppo', length: 3, options: { optInFastPeriod: 3, optInSlowPeriod: 2, optInMAType: 0 } });
+    this.ppoAlt = new TALIBASYNC({ indicator: 'ppo', length: 500, options: { optInFastPeriod: 3, optInSlowPeriod: 2, optInMAType: 0 } });
+    this.ppoBtc = new TALIBASYNC({ indicator: 'ppo', length: 500, options: { optInFastPeriod: 3, optInSlowPeriod: 2, optInMAType: 0 } });
 }
 
 
-stratFlash.onCandle = async function (candle) {
+// ***************************************************************************
+// * 1 Min. altcoin candle event
+stratMM.onCandle = async function (candle, check=true) {
     //read candle for second market (btc)
-    return new Promise((resolve, reject) => {
-        var future = candle.start.add(1, 'second').unix();
-        var start = candle.start.subtract(1, 'second').unix();
+    var future = candle.start.add(1, 'second').unix();
+    var start = candle.start.subtract(1, 'second').unix();
+    
+    //get foreign candle from db, similar to gekko leecher mode
+    this.reader.get(start, future, 'full', async (err, btcCandles) => {
+        var amount = _.size(btcCandles);
         
-        //get foreign candle from db, similar to gekko leecher mode
-        this.reader.get(start, future, 'full', async (err, candles) => {
-            var amount = _.size(candles);
-            
-            if(amount === 0) {
-               log.debug('no new BTC candles');
-               resolve();
-               return;
-            }
-
-            candles[0].start = moment.unix(candles[0].start); //convert from unix epoch to moment
-            
-            this.ppoFC.result = await this.ppoFC.update(candles[0]);
-            this.ppo.result = await this.ppo.update(candle);
-            
-            if (this.ppoFC.result >= 0.4 && this.ppo.result < this.ppoFC.result) {
-                log.debug('\n\n', 'ETH:', candle.close, candle.start.format() ,this.ppo.result,' ::: BTC:', candles[0].close, candles[0].start.format(), this.ppoFC.result);
-                this.advice({
-                    direction: 'long',
-                    trigger: {
-                    type: 'trailingStop',
-                    trailPercentage: 0.1
-                    }
-                });
-             }
-            
+        if(amount === 0) {
+            log.debug('no BTC candles');
             resolve();
-        }, 'candles_usdt_btc');
-    });
+            return;
+        }
+
+        btcCandles[0].start = moment.unix(btcCandles[0].start); //convert from unix epoch to moment
+        
+        this.ppoBtc.result = await this.ppoBtc.update(btcCandles[0]);
+        this.ppoAlt.result = await this.ppoAlt.update(candle);
+        
+        if (this.ppoBtc.result >= 0.4 && this.ppoAlt.result < this.ppoBtc.result && !this.exposedMM && check) {
+            //log.debug('\n\n', 'ETH:', candle.close, candle.start.format() ,this.ppoAlt.result,' ::: BTC:', btcCandles[0].close, btcCandles[0].start.format(), this.ppoBtc.result);
+            log.debug('BUY with multimarket strategy...');
+            this.exposedMM = true;
+            this.advice({
+                direction: 'long',
+                trigger: {
+                    type: 'trailingStop',
+                    strategy: 'multimarket',
+                    trailPercentage: 1.0
+                }
+            });
+        }
+        
+        if (this.ppoBtc.result < -0.3) {  
+            this.btcFlashcrash = true;
+        } else {
+            this.btcFlashcrash = false;
+        }
+    }, 'candles_usdt_btc');
 }
 
 
-stratFlash.update = function (candle) {
+// ***************************************************************************
+// * Catch trade event completed and store expose info
+stratMM.onTrade = function (trade) {
+   if (trade.action == 'buy' && trade.trigger.strategy == 'multimarket') this.exposedMM = true;
+   if (trade.action == 'sell' && trade.trigger.strategy == 'multimarket') this.exposedMM = false;
+}
+
+
+stratMM.update = function (candle) {
     //no need for strat update, we work with onCandle custom batching
 }
 
 
-stratFlash.check = function (candle) {
+stratMM.check = function (candle) {
    //no need for strat check, we work with onCandle custom batching and strat execution
 }
 
 
-module.exports = stratFlash;
+module.exports = stratMM;
