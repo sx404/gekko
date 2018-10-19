dafc40d3ec53ae0ba91fdc142b8b16b8bb821fb0
mark.sch <mark@schmalohr.de>
2018-10-19 18:43:59 +0200

    Write candles without cache delay to db

--- plugins/postgresql/writer.js
+++ plugins/postgresql/writer.js
@@ -53,7 +53,7 @@ Store.prototype.writeCandles = function() {
 
 var processCandle = function(candle, done) {
   this.cache.push(candle);
-  if (this.cache.length > 1)
+  if (this.cache.length > 0)
     this.writeCandles();
 
   done();
