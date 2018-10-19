da5cbb6d37bcc11af31b86f4739ed3934d523325
mark.sch <mark@schmalohr.de>
2018-10-19 18:16:50 +0200

    Added runtime and memory measurement to gekko backtest

--- backtest2html.sh
+++ backtest2html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node gekko.js --config config-eth.js --backtest --set debug=true | aha --black --title 'T5strat_ETHEUR_20180101-20181001' > backtest-results/T5strat_ETHEUR_20180101-20181001.html
+/usr/bin/time --verbose node gekko.js --config config-eth.js --backtest --set debug=true | aha --black --title 'T5forecast_ETHEUR_20180101-20181001' > backtest-results/T5forecast_ETHEUR_20180101-20181001.html
