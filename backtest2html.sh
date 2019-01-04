4fc82950d429890eaa8a4fe59c6faf04e850aaab
mark-sch <mark@schmalohr.de>
2019-01-04 17:27:09 +0100

    Added T5mainasync and T5multimarket-xt backtests

--- backtest2html.sh
+++ backtest2html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/time --verbose node gekko.js --config config-eth.js --backtest --set debug=true | aha --black --title 'T5forecast_ETHEUR_20180101-20181226' > backtest-results/T5forecast_ETHEUR_20180101-20181226.html
+/usr/bin/time --verbose node gekko.js --config sample-eth.js --backtest --set debug=true | aha --black --title 'T5mainasync_ETHEUR_20180101-20181231' > backtest-results/T5mainasync_ETHEUR_20180101-20181231.html
